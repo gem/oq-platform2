59f86ad19e08f60c31be5b913b9fcb37282cf5d1
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-02-10 08:46:27 +0100

    modified command for installation

--- verifier.sh
+++ verifier.sh
@@ -1,69 +1,61 @@
 #!/bin/bash
 
-set -x
+# Inside the LXC
 
-#install essential lib and python
-sudo apt-get update
-sudo apt-get install -y build-essential libxml2-dev libxslt1-dev libpq-dev zlib1g-dev
-sudo apt-get install -y python-dev python-imaging python-lxml python-pyproj python-shapely python-nose python-httplib2 python-pip python-virtualenv python-software-properties
+## Pre-requisites
+sudo apt update
+sudo apt install -y git python-dev python-virtualenv libpq-dev libgdal-dev openjdk-8-jdk-headless
 
-#install and configuration postgres
-sudo apt-get install -y postgresql-9.3-postgis-2.1 postgresql-9.3-postgis-scripts
-sudo -u postgres createdb geonode_dev
-sudo -u postgres createdb geonode_dev-imports
-cat << EOF | sudo -u postgres psql
-CREATE USER geonode_dev WITH PASSWORD 'geonode_dev';
-GRANT ALL PRIVILEGES ON DATABASE "geonode_dev" to geonode_dev;
-GRANT ALL PRIVILEGES ON DATABASE "geonode_dev-imports" to geonode_dev;
+## Create the virtualenv
+virtualenv ~/env
+source ~/env/bin/activate
+
+## Clone GeoNode
+git clone https://github.com/GeoNode/geonode
+
+## Install GeoNode and dependencies
+cd geonode
+pip install -e .
+pip install pygdal==1.11.3.3
+
+## Set the local_settings.py
+IP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+cat << EOF > geonode/local_settings.py
+import os
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+SITEURL = 'http://$IP:8000'
+ALLOWED_HOSTS = ['$IP', 'localhost']
+
+GEOSERVER_LOCATION = 'http://$IP:8080/geoserver/'
+GEOSERVER_PUBLIC_LOCATION = 'http://$IP:8080/geoserver/'
+OGC_SERVER = {
+    'default': {
+        'BACKEND': 'geonode.geoserver',
+        'LOCATION': GEOSERVER_LOCATION,
+        'LOGIN_ENDPOINT': 'j_spring_oauth2_geonode_login',
+        'LOGOUT_ENDPOINT': 'j_spring_oauth2_geonode_logout',
+        # PUBLIC_LOCATION needs to be kept like this because in dev mode
+        # the proxy won't work and the integration tests will fail
+        # the entire block has to be overridden in the local_settings
+        'PUBLIC_LOCATION': GEOSERVER_PUBLIC_LOCATION,
+        'USER' : 'admin',
+        'PASSWORD' : 'geoserver',
+        'MAPFISH_PRINT_ENABLED' : True,
+        'PRINT_NG_ENABLED' : True,
+        'GEONODE_SECURITY_ENABLED' : True,
+        'GEOGIG_ENABLED' : False,
+        'WMST_ENABLED' : False,
+        'BACKEND_WRITE_ENABLED': True,
+        'WPS_ENABLED' : False,
+        'LOG_FILE': '%s/geoserver/data/logs/geoserver.log' % os.path.abspath(os.path.join(PROJECT_ROOT, os.pardir)),
+        # Set to dictionary identifier of database containing spatial data in DATABASES dictionary to enable
+        'DATASTORE': '', #'datastore',
+    }
+}
 EOF
 
-sudo -u postgres psql -d geonode_dev-imports -c 'CREATE EXTENSION postgis;'
-sudo -u postgres psql -d geonode_dev-imports -c 'GRANT ALL ON geometry_columns TO PUBLIC;'
-sudo -u postgres psql -d geonode_dev-imports -c 'GRANT ALL ON spatial_ref_sys TO PUBLIC;'
-
-#insert line in pg_hba.conf postgres
-sudo sed -i '1 local   all             all                                trust' /etc/postgresql/9.3/main/pg_hba.conf
-
-#restart postgres
-sudo service postgresql restart
-
-#install java8
-sudo apt-get install -y software-properties-common
-sudo apt-get install python-software-properties
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo apt-get update
-echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-sudo apt-get install -y oracle-java8-installer
-sudo apt-get install -y git gettext libgdal-dev
-
-#install pip
-sudo apt-get -y install python-pip
-sudo pip install --upgrade pip
-
-#create folder and virtual env
-mkdir ~/dev
-cd ~/dev
-virtualenv env
-source env/bin/activate
-pip install pip -U
-
-#install gdal
-export CPLUS_INCLUDE_PATH=/usr/include/gdal
-export C_INCLUDE_PATH=/usr/include/gdal
-pip install gdal==1.10.0
-
-#clone geonode from official repo github
-git clone -b 2.5.x https://github.com/GeoNode/geonode.git
-cd ~/dev/geonode/geonode
-cp -a local_settings.py.geoserver.sample local_settings.py
-
-#set and start geonode
-cd ~/dev/geonode
-pip install -r requirements.txt
-pip install -e .
-pip install psycopg2==2.4.5
-python manage.py syncdb --noinput
+## Run GeoNode
 paver setup
 paver sync
 paver start -b 0.0.0.0:8000
-
