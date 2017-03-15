b68594aff5b4cb6bbf427f5e50e26fcfb1937a60
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-03-15 15:25:18 +0100

    increased time sleep

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -77,7 +77,7 @@ cd ~/geonode
 paver setup
 paver sync
 paver start -b 0.0.0.0:8000
-sleep 90
+sleep 4000
 # paver stop
 # kill $(ps ax | grep 'python manage.py runserver ' | grep -v 'grep' | sed 's/^ *//g;s/ .*//g' | tr '\n' ' ')
 
