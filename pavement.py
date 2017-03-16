026f2c6dd4103d9b3a4b71e4d01c5734e0d74c20
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-03-16 16:19:26 +0100

    changed pavement py and created local_settings tmpl

--- pavement.py
+++ pavement.py
@@ -1,5 +1,4 @@
-import sys
-import os
+mport os
 import re
 import shutil
 import sys
@@ -11,21 +10,30 @@ import glob
 import fileinput
 import yaml
 
-LXC_IP = sys.argv[1]
-GEM_LOCAL_SETTINGS_TMPL = 'geonode/geonode/local_settings.py.template'
+from setuptools.command import easy_install
+from urlparse import urlparse
+
+from paver.easy import task, options, cmdopts, needs
+from paver.easy import path, sh, info, call_task
+from paver.easy import BuildFailure
+
+
+GEM_LOCAL_SETTINGS_TMPL = os.path.join(os.path.expanduser("~"),
+                                           'geonode', 'local_settings.py.tmpl')
 def _write_local_settings(lxc_ip):
     local_settings = open(GEM_LOCAL_SETTINGS_TMPL, 'r').read()
-    with open('geonode/geonode/local_settings.py', 'w') as fh:
-        fh.write(local_settings % dict(lxc_ip=LXC_IP))
+    with open(os.path.join(os.path.expanduser("~"),
+                               'geonode', 'local_settings.py'), 'w') as fh:
+        fh.write(local_settings % dict(lxc_ip=lxc_ip
+                                       ))
 
 @task
-@needs(['_write_local_settings'])
 @cmdopts([
-    ('bind=', 'b', 'Bind server to provided IP address and port number.')
+    ('lxc_ip=', 'l', 'Bind server to provided IP address and port number.')
 ])
-def custom_local():
-    """
-    Start GeoNode (Django, GeoServer & Client)
-    """
+def setup():
+    lxc_ip = options.get('lxc_ip', '')
+    # info(lxc_ip)
+    _write_local_settings(lxc_ip)
     info("Local setting changed.")
 
