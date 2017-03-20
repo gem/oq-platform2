219958a32a035e3b1461aa0e462fd23515979402
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-03-20 12:16:07 +0100

    added join for ~

--- pavement.py
+++ pavement.py
@@ -18,14 +18,15 @@ from paver.easy import path, sh, info, call_task
 from paver.easy import BuildFailure
 
 
-#GEM_LOCAL_SETTINGS_TMPL = os.path.join(os.path.expanduser("~"),
-#                                           'geonode/geonode', 
-#                                           'local_settings.py.tmpl')
+GEM_LOCAL_SETTINGS_TMPL = os.path.join(os.path.expanduser("~"),
+                                           'geonode/geonode', 
+                                           'local_settings.py.tmpl')
 
-GEM_LOCAL_SETTINGS_TMPL = 'geonode/geonode/local_settings.py.tmpl'
+#GEM_LOCAL_SETTINGS_TMPL = 'geonode/geonode/local_settings.py.tmpl'
 def _write_local_settings(lxc_ip):
     local_settings = open(GEM_LOCAL_SETTINGS_TMPL, 'r').read()
-    with open('geonode/geonode/local_settings.py', 'w') as fh:
+    with open(os.path.join(os.path.expanduser("~"), 'geonode/geonode',
+                               'local_settings.py.tmpl'), 'w') as fh:
         fh.write(local_settings % dict(lxc_ip=lxc_ip
                                        ))
 
