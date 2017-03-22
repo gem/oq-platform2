ad1619c2a16495a1e7fbb0286f44b6c1ef52d178
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-03-22 14:48:25 +0100

    optimized code

--- pavement.py
+++ pavement.py
@@ -1,14 +1,4 @@
 import os
-import re
-import shutil
-import sys
-import time
-import urllib
-import urllib2
-import zipfile
-import glob
-import fileinput
-import yaml
 
 from setuptools.command import easy_install
 from urlparse import urlparse
@@ -18,14 +8,16 @@ from paver.easy import path, sh, info, call_task
 from paver.easy import BuildFailure
 
 
-#GEM_LOCAL_SETTINGS_TMPL = os.path.join(os.path.expanduser("~"),
-#                                           'local_settings.py.tmpl')
-
 GEM_LOCAL_SETTINGS_TMPL = 'local_settings.py.tmpl'
+
+
 def _write_local_settings(lxc_ip):
     local_settings = open(GEM_LOCAL_SETTINGS_TMPL, 'r').read()
-    with open('geonode/geonode/local_settings.py', 'w') as fh:
+    with open(os.path.join(os.path.expanduser("~"), 'geonode/geonode/'
+                                                    'local_settings'
+                                                    '.py'), 'w') as fh:
         fh.write(local_settings % dict(lxc_ip=lxc_ip))
+
 
 @task
 @cmdopts([
