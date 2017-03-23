fb0578e50982fae142a27041e5cdf66aa54b23a0
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-03-23 14:23:40 +0100

    added folder openquakeplatform for tests

---
+++ openquakeplatform/test/irv_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import unittest
+
+from openquakeplatform.test import pla
+
+
+class IrvTest(unittest.TestCase):
+    def irv_test(self):
+        # go to test page
+        pla.get('/irv/test/')
+
+        # wait DOM population via async JS
+        pla.xpath_finduniq(
+            "//div[@class='jasmine_html-reporter']/div"
+            "[@class='results']/div[@class='summary']",
+            100, 1)
+
+        # check the result of tests
+        pla.xpath_finduniq(
+            "//span[@class='bar passed' and contains"
+            "(normalize-space(text()), ', 0 failures')]")
