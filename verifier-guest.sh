7a45451f0b98185bf29a6588db1510361c76fa60
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-03-27 16:08:26 +0200

    deleted code cat for changed local_settings.py

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -62,44 +62,6 @@ cd ~
 # Create a symbolic link in your virtualenv
 ln -s /usr/lib/python2.7/dist-packages/osgeo env/lib/python2.7/site-packages/osgeo
 
-## Set the local_settings.py
-#IP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
-#cat << EOF > geonode/local_settings.py
-#import os
-#PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-
-#SITEURL = 'http://$IP:8000'
-#ALLOWED_HOSTS = ['$IP', 'localhost']
-
-#GEOSERVER_LOCATION = 'http://$IP:8080/geoserver/'
-#GEOSERVER_PUBLIC_LOCATION = 'http://$IP:8080/geoserver/'
-#OGC_SERVER = {
-#    'default': {
-#        'BACKEND': 'geonode.geoserver',
-#        'LOCATION': GEOSERVER_LOCATION,
-#        'LOGIN_ENDPOINT': 'j_spring_oauth2_geonode_login',
-#       'LOGOUT_ENDPOINT': 'j_spring_oauth2_geonode_logout',
-        # PUBLIC_LOCATION needs to be kept like this because in dev mode
-        # the proxy won't work and the integration tests will fail
-        # the entire block has to be overridden in the local_settings
-#        'PUBLIC_LOCATION': GEOSERVER_PUBLIC_LOCATION,
-#        'USER' : 'admin',
-#        'PASSWORD' : 'geoserver',
-#        'MAPFISH_PRINT_ENABLED' : True,
-#        'PRINT_NG_ENABLED' : True,
-#        'GEONODE_SECURITY_ENABLED' : True,
-#        'GEOGIG_ENABLED' : False,
-#        'WMST_ENABLED' : False,
-#        'BACKEND_WRITE_ENABLED': True,
-#        'WPS_ENABLED' : False,
-#        'LOG_FILE': '%%s/geoserver/data/logs/geoserver.log' %% os.path.abspath(os.path.join(PROJECT_ROOT, os.pardir)),
-#        # Set to dictionary identifier of database containing spatial data in DATABASES dictionary to enable
-#        'DATASTORE': '', #'datastore',
-#    }
-#}
-#EOF
-
-
 sudo cp -Rf $HOME/"$GIT_REPO"/html/* $HOME/
 sudo cp $HOME/"$GIT_REPO"/urls.py $HOME/geonode/geonode
 
