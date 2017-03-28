7bc6f2aa051690d4738e92e46257ba855c286b43
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-03-28 16:04:41 +0200

    adde on cmdopts webui with 'w' for pavement setup that modify local_settings.py

--- pavement.py
+++ pavement.py
@@ -24,6 +24,7 @@ def _write_local_settings(lxc_ip,webuiurl):
 @task
 @cmdopts([
     ('lxc_ip=', 'l', 'Bind server to provided IP address and port number.')
+    ('webuiurl=', 'w', 'Bind server to provided URL of webui.')
 ])
 def setup():
     lxc_ip = options.get('lxc_ip', '')
