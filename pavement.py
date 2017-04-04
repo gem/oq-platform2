19604a1736701b8a146c0dfe283599fcfb456a47
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-04-04 10:54:30 +0200

    added datadir in pavement.py and FILE_PATH_FIELD_DIRECTORY in local_settings.py.tmpl

--- pavement.py
+++ pavement.py
@@ -11,14 +11,15 @@ from paver.easy import BuildFailure
 GEM_LOCAL_SETTINGS_TMPL = 'local_settings.py.tmpl'
 
 
-def _write_local_settings(lxc_ip, webuiurl):
+def _write_local_settings(lxc_ip, webuiurl, datadir):
     local_settings = open(GEM_LOCAL_SETTINGS_TMPL, 'r').read()
     with open(os.path.join(os.path.expanduser("~"), 'oq-platform2/'
                                                     'openquakeplatform/'
                                                     'local_settings'
                                                     '.py'), 'w') as fh:
         fh.write(local_settings % dict(lxc_ip=lxc_ip,
-                                       webuiurl=webuiurl
+                                       webuiurl=webuiurl,
+                                       datadir=datadir
                                        ))
 
 
@@ -30,8 +31,9 @@ def _write_local_settings(lxc_ip, webuiurl):
 def setup():
     lxc_ip = options.get('lxc_ip', '')
     webuiurl = options.get('webuiurl', '')
+    datadir = options.get('datadir', 'data/')
     # info(lxc_ip)
-    _write_local_settings(lxc_ip, webuiurl)
+    _write_local_settings(lxc_ip, webuiurl, datadir)
     info("Local setting changed.")
 
 
