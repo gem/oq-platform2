fd5272df3c3cd24fab35d2688e63b6cab261cc69
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-08-25 11:01:16 +0200

    changed folder os join path and added pdb

--- openquakeplatform/test/shape_test.py
+++ openquakeplatform/test/shape_test.py
@@ -8,11 +8,12 @@ from selenium.webdriver.common.keys import Keys
 class ShapeTest(unittest.TestCase):
 
     def upload_shape_test(self):
-        # sph = os.path.join(os.path.expanduser("~"), 'oq-platform2',
-        #                                            'openquakeplatform',
-        #                                            'test/shapefile',
-        #                                            'exampleshape.zip')
-        sph = "~/oq-platform2/openquakeplatform/test/shapefile/exampleshape.zip"
+        sph = os.path.join(os.path.expanduser("~"), 'oq-platform2',
+                                                    'openquakeplatform',
+                                                    'test',
+                                                    'shapefile',
+                                                    'exampleshape.zip')
+        import pdb ; pdb.set_trace()
 
         pla.get('')
 
