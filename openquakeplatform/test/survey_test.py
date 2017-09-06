d86c164cbfe86a993fd0d987f6001555437c8d6d
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-09-06 16:52:32 +0200

    added othe tests in suvery_test.py

--- openquakeplatform/test/survey_test.py
+++ openquakeplatform/test/survey_test.py
@@ -23,8 +23,68 @@ class SurveyTest(unittest.TestCase):
                "//select[@id='country-id']/option[text()='Anguilla']",
                100, 1)
         select.click()
-      
+
         choice_new = pla.xpath_finduniq(
-                   "//button[@id='new-classification']", 
+                   "//button[@id='new-classification']",
                    100, 1)
         choice_new.click()
+
+        occupancy = pla.xpath_finduniq(
+                  "//input[@value='industrial' and @type='radio']")
+        occupancy.click()
+
+        choice_next = pla.xpath_finduniq(
+                    "//button[@name='next']",
+                    100, 1)
+        choice_next.click()
+
+        choice_material = pla.xpath_finduniq(
+                        "//input[@name='steel' and @type='checkbox']",
+                        100, 1)
+        choice_material.click()
+
+        choice_steel = pla.xpath_finduniq(
+                     "//input[@name='Cold formed members'"
+                     " and @type='checkbox']",
+                     100, 1)
+        choice_steel.click()
+
+        type_conn = pla.xpath_finduniq(
+                  "//input[@name='Riveted' and @type='checkbox']",
+                  100, 1)
+        type_conn.click()
+
+        llrsyst = pla.xpath_finduniq(
+                "//input[@name='Wall' and @type='checkbox']",
+                100, 1)
+        llrsyst.click()
+
+        height = pla.xpath_finduniq(
+               "//input[@name='High-rise (7-12 floors)'"
+               " and @type='checkbox']",
+               100, 1)
+        height.click()
+ 
+        irreg = pla.xpath_finduniq(
+              "//input[@name='Regular' and @type='checkbox']",
+              100, 1)
+        irreg.click() 
+
+        duct = pla.xpath_finduniq(
+             "//input[@name='Non ductile (PGA<0.1g)' and @type='checkbox']",
+             100, 1)
+
+        duct.click()
+
+        save = pla.xpath_finduniq(
+             "//button[@name='save']",
+             100, 1)
+        save.click()
+
+        # result
+        pla.xpath_finduniq(
+            "//div[normalize-space(text())='success'"
+            " and @class='save_resp_ok'],"
+            100, 1) 
+
+
