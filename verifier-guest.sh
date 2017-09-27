207a5b6ec97b51602babb85199027e4e4b777066
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-09-27 14:40:34 +0000

    commented django_setting_module

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -24,7 +24,7 @@ GEO_STABLE_HASH="1c65c9b"
 geonode_setup_env()
 {
     export PYTHONPATH=$HOME/oq-platform2:$HOME/oq-platform-taxtweb:$HOME/oq-platform-ipt
-    export DJANGO_SETTINGS_MODULE='openquakeplatform.settings'
+    #export DJANGO_SETTINGS_MODULE='openquakeplatform.settings'
     export LOCKDOWN_GEONODE='true'
 }
 
