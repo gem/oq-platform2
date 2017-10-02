d48689ef470aa81b8c0652e77e255a622ad84953
hascar <armando.scarpati@globalquakemodel.org>
2017-10-02 12:50:47 +0000

    fixed d3.js and changed path rel js in site_base.html

--- openquakeplatform/static/geonode/js/d3.js
+++ openquakeplatform/static/geonode/js/d3.js
@@ -9501,8 +9501,4 @@
   });
   if (typeof define === "function" && define.amd) define(d3); else if (typeof module === "object" && module.exports) module.exports = d3;
   this.d3 = d3;
-<<<<<<< HEAD
 }();
-=======
-}();
->>>>>>> origin
