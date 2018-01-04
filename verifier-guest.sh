7e8f0b3c2911de772b5b72100335ca79cf9e1250
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2018-01-04 07:44:28 +0000

    changed name function nested_exec with extra_deps_install in verifier-guest

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -29,8 +29,8 @@ geonode_setup_env()
     export LOCKDOWN_GEONODE='true'
 }
 
-##function nested applicatione
-nested_exec() {
+#install dependencies vulnerability and ipt
+extra_deps_install() {
     python -m pip install "django<2"
     pip install django-nested-inline
     pip install django_extras
@@ -100,8 +100,8 @@ fi
 
 source ~/env/bin/activate
 
-## install nested applications
-nested_exec
+# install nested applications
+extra_deps_install
 
 pip install scipy
 
