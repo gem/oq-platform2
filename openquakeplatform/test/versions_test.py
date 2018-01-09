2d81cbb647ab04e73529ee8f05ee65b9ae267976
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2018-01-09 16:05:25 +0000

    fixed flake error for versions_test.py

--- openquakeplatform/test/versions_test.py
+++ openquakeplatform/test/versions_test.py
@@ -7,8 +7,6 @@ class AppVersionsTest(unittest.TestCase):
 
     def page_app_versions_test(self):
 
-        # pla_get('localhost:8000')
-
         versions_url = '/versions'
 
         # redirect page of versions application
@@ -21,6 +19,10 @@ class AppVersionsTest(unittest.TestCase):
         # macthing if the version of applications
         # content numbers and points
         search_version_app = pla.xpath_finduniq(
-            "//table[@class='versions_table']//tr/td[../td[normalize-space(text())='IPT']][normalize-space(text())!='IPT']")
+            "//table[@class='versions_table']//tr/td[../"
+            "td[normalize-space(text())='IPT']]"
+            "[normalize-space(text())!='IPT']")
         vers = search_version_app.text
-        self.assertRegexpMatches(vers, '^[0-9]+\.', msg=("version [%s] not valid for IPT app" % vers))
+        self.assertRegexpMatches(vers, '^[0-9]+\.', msg=("version [%s] not"
+                                                         " valid for IPT"
+                                                         " app" % vers))
