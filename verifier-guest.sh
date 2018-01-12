91118e36af2aa0269a5163d4cc74154966744138
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2018-01-12 15:32:32 +0100

    added export GEM_OPT_PACKAGES before lauch tests in verifier-guest

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -59,9 +59,11 @@ exec_test () {
     # cd $GIT_REPO
     export PYTHONPATH=$HOME/oq-moon:$HOME/$GIT_REPO:$HOME/$GIT_REPO/openquakeplatform/test/config:$HOME/oq-platform-taxtweb:$HOME/oq-platform-ipt
 
+    export GEM_OPT_PACKAGES="$(python -c 'from openquakeplatform.settings import STANDALONE_APPS ; print(",".join(x for x in STANDALONE_APPS))')"
+
     export DISPLAY=:1
     python -m openquake.moon.nose_runner --failurecatcher dev -s -v --with-xunit --xunit-file=xunit-platform-dev.xml $GIT_REPO/openquakeplatform/test # || true
-    # sleep 40000 || true
+
 }
 
 rem_sig_hand() {
