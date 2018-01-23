530d60be92479ec5ffd58e97538df22d28451070
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2018-01-23 11:23:01 +0100

    fixed flake error and added new import from moon with platform_get() in versions_test.py

--- openquakeplatform/test/versions_test.py
+++ openquakeplatform/test/versions_test.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 import unittest
-from openquakeplatform.test import pla
+from openquake.moon import platform_get
 
 
 class AppVersionsTest(unittest.TestCase):
 
     def page_app_versions_test(self):
 
-        # pla_get('localhost:8000')
+        pla = platform_get()
 
         versions_url = '/versions'
 
@@ -21,6 +21,11 @@ class AppVersionsTest(unittest.TestCase):
         # macthing if the version of applications
         # content numbers and points
         search_version_app = pla.xpath_finduniq(
-            "//table[@class='versions_table']//tr/td[../td[normalize-space(text())='IPT']][normalize-space(text())!='IPT']")
+            "//table[@class='versions_table']//tr/"
+            "td[../td[normalize-space(text())"
+            "='IPT']][normalize-space(text())!='IPT']")
         vers = search_version_app.text
-        self.assertRegexpMatches(vers, '^[0-9]+\.', msg=("version [%s] not valid for IPT app" % vers))
+        self.assertRegexpMatches(vers, '^[0-9]+\.', msg=("version [%s]"
+                                                         " not valid"
+                                                         " for IPT"
+                                                         " app" % vers))
