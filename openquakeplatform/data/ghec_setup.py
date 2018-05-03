4039122fd183951cdec1cd5fd7875d7052a71e49
hascar <armando.scarpati@globalquakemodel.org>
2018-05-03 15:21:22 +0000

    used importlib to import arguments in ghec_setup

--- openquakeplatform/data/ghec_setup.py
+++ openquakeplatform/data/ghec_setup.py
@@ -1,48 +1,41 @@
 #!/usr/bin/env python
-
+import sys
+import importlib
 from geoserver.catalog import Catalog
 import geoserver.util
-from ghec import (folder_rest,
-                  name_workspace,
-                  folder_workspace,
-                  name,
-                  name_store,
-                  name_layer,
-                  title,
-                  abstract,
-                  keywords,
-                  folder_shape)
 
-cat = Catalog("%s" % folder_rest, "admin", "geoserver")
+mod = importlib.import_module(sys.argv[1])
 
-dir_shapefile = geoserver.util.shapefile_and_friends("%s/%s" % (folder_shape,
-                                                                name))
-workspace = cat.get_workspace("%s" % name_workspace)
+cat = Catalog("%s" % mod.folder_rest, "admin", "geoserver")
+
+dir_shapefile = geoserver.util.shapefile_and_friends("%s/%s" % (mod.folder_shape,
+                                                                mod.name))
+workspace = cat.get_workspace("%s" % mod.name_workspace)
 
 if workspace:
     pass
 else:
-    ws = cat.create_workspace(name_workspace, '%s' % folder_workspace)
+    ws = cat.create_workspace(mod.name_workspace, '%s' % mod.folder_workspace)
 
-with open("%s/%s.sld" % (folder_shape, name)) as f:
-    cat.create_style(name, f.read(), overwrite=True)
+with open("%s/%s.sld" % (mod.folder_shape, mod.name)) as f:
+    cat.create_style(mod.name, f.read(), overwrite=True)
 
-fs = cat.create_featurestore("%s" % name, dir_shapefile, workspace)
+fs = cat.create_featurestore("%s" % mod.name, dir_shapefile, workspace)
 
-layer = cat.get_layer("%s" % name_layer)
-layer._set_default_style("%s" % name_store)
+layer = cat.get_layer("%s" % mod.name_layer)
+layer._set_default_style("%s" % mod.name_store)
 cat.save(layer)
 
-resource = cat.get_resource("%s" % name, workspace="%s" % name_workspace)
+resource = cat.get_resource("%s" % mod.name, workspace="%s" % mod.name_workspace)
 # print dir(resource)
-resource.title = "%s" % title
-resource.abstract = "%s" % abstract
-resource.keywords = keywords
+resource.title = "%s" % mod.title
+resource.abstract = "%s" % mod.abstract
+resource.keywords = mod.keywords
 # resource.metadata_links = [('text/xml', 'other', '%s.xml' % name),]
 cat.save(resource)
 
 # output
 if layer:
-    print "Updated layer %s" % name_layer
+    print "Updated layer %s" % mod.name_layer
 else:
-    print "Created layer %s" % name_layer
+    print "Created layer %s" % mod.name_layer
