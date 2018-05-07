da1cf5a80f204fd8efd1a6ef090514e3cfbe20f3
hascar <armando.scarpati@globalquakemodel.org>
2018-05-07 08:47:38 +0000

    added py with metadata isc_viewer_mesure2

--- openquakeplatform/data/isc.py
+++ openquakeplatform/data/isc.py
@@ -8,18 +8,16 @@ name_workspace = "geonode"
 
 folder_workspace = "http://localhost:8080/geoserver/rest/workspaces"
 
-name = "isc-gem"
+name = "isc_viewer_measure2"
 
 title = "ISC-GEM Catalogue v2"
 
-abstract = "events with magnitude >= 7.5, occurred between 1900 and 1917
-            events with magnitude >= 6.25, occurred between 1918 and 1949
-            events with magnitude >= 5.5, occurred between 1950 and 2011"
+abstract = "events with magnitude >= 7.5, occurred between 1900 and 1917 events with magnitude >= 6.25, occurred between 1918 and 1949 events with magnitude >= 5.5, occurred between 1950 and 2011"
 
 keywords = ['GC', 'GEM-supplemental-layer', 'ISC', 'catalogue', 'component', 'event', 'global', 'instrumental']
 
-name_layer = "geonode:isc-gem"
+name_layer = "geonode:isc_viewer_measure2"
 
-name_store = "isc-gem"
+name_store = "isc_viewer_measure2"
 
-folder_shape = "/home/ubuntu/oq-private/isc-gem2/"
+folder_shape = "/home/ubuntu/oq-private/isc_data"
