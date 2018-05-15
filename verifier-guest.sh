432e773955d8dda4ed415cb5f561048bfd7f7eb9
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2018-05-15 14:35:18 +0200

    added -f to oqsetup paver

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -188,12 +188,11 @@ geonode_setup_env
 ## Sync and setup GeoNode
 cd ~/geonode
 
-paver oqsetup -l $LXC_IP -u localhost:8800 -s /home/ubuntu/geonode/data
 paver -f $HOME/$GIT_REPO/pavement.py setup
 
 ## modify local_settings with pavement from repo
 # cd ~/oq-platform2
-# paver oqsetup -l $LXC_IP -u localhost:8800 -s /home/ubuntu/geonode/data
+paver -f $HOME/oq-platform2/pavement.py oqsetup -l $LXC_IP -u localhost:8800 -s /home/ubuntu/geonode/data
 
 # cd ~/geonode
 python manage.py migrate account --noinput
