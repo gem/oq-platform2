5a15625a97cfc7603b97ab018aaceb1b97031f78
Matteo Nastasi <nastasi@openquake.org>
2018-05-24 10:56:39 +0200

    update isc test to manage map in the platform2 context (now map id is retrieved dynamically)

--- openquakeplatform/test/isc_test.py
+++ openquakeplatform/test/isc_test.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import unittest
-
+import os
 from openquake.moon import platform_get
 
-@unittest.skip("temporarily disabled")
+
 class IscTest(unittest.TestCase):
     def isc_test(self):
 
@@ -17,38 +17,41 @@ class IscTest(unittest.TestCase):
         # <li>
         # <a href="/maps/23">
         enter_button = pla.xpath_finduniq(
-            "//li/a[@href='/maps/23' and normalize-space(text()) = 'Global "
+            "//li/a[normalize-space(text()) = 'Global "
             "Instrumental Earthquake Catalogue (1900 - 2009)']")
+
+        href = enter_button.get_attribute('href')
+        id_map = os.path.basename(href)
         enter_button.click()
-        pla.wait_new_page(enter_button, '/maps/23')
+        pla.wait_new_page(enter_button, '/maps/%s' % id_map)
 
         enter_button = pla.xpath_finduniq(
-            "//a[@href='/maps/23/view' and "
-            "normalize-space(text()) = 'View Map']")
+            "//a[@href='/maps/%s/view' and "
+            "normalize-space(text()) = 'View Map']" % id_map)
         enter_button.click()
-        pla.wait_new_page(enter_button, '/maps/23/view', timeout=15)
+        pla.wait_new_page(enter_button, '/maps/%s/view' % id_map, timeout=15)
 
         # <button id="ext-gen159" class=" x-btn-text gxp-icon-getfeatureinfo"
         # type="button">Identify
         enter_button = pla.xpath_finduniq(
             "//button[@type='button' and normalize-space(text())"
-            "= 'Identify']", 50)
+            "= 'Identify']", timeout=15)
         enter_button.click()
 
         # wait info button will be clicked
         pla.xpath_finduniq(
             "//button[@type='button' and normalize-space(text())"
             "= 'Identify']/../../../../..[contains(concat(' ', @class, ' '),"
-            " ' x-btn-pressed ')]", 100)
+            " ' x-btn-pressed ')]", timeout=15)
 
         _, x, y = pla.xpath_finduniq_coords(
-            "//img[contains(@src, 'wms?LAYERS=oqplatform%3Aisc_viewer_measure"
-            "&FORMAT=image%2Fpng&TRANSPARENT=TRUE&SERVICE=WMS&VERSION=1.1.1"
-            "&REQUEST=GetMap&STYLES=&TILED=true&SRS=EPSG%3A900913&BBOX="
-            "-10018754.17,0,-5009377.085,5009377.085&WIDTH=256&HEIGHT=256')]",
-            50)
+            "//img[contains(@src, 'LAYERS=oqplatform%3Aisc_viewer_measure"
+            "&FORMAT=image%2Fpng&TRANSPARENT=TRUE&SERVICE=WMS&VERSION="
+            "1.1.1&REQUEST=GetMap&STYLES=&TILED=true&SRS=EPSG%3A900913&"
+            "BBOX=-5009377.085,0,0,5009377.085&WIDTH=256&HEIGHT=256')]",
+            timeout=50)
 
         pla.add_click_event()
-        pla.click_at(107 + x, 115 + y)
+        pla.click_at(107 + x, 70 + y)
 
-        pla.xpath_finduniq("//div[text() = '1951-03-19T04:23:00']", 50)
+        pla.xpath_finduniq("//div[text() = '1942-02-09T12:11:00']", timeout=50)
