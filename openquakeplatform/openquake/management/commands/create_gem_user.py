72b1e5faaaae3eb39535b8401a240839e5eeb266
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2018-06-04 17:17:36 +0200

    fixed flake error and added some comment

--- openquakeplatform/openquake/management/commands/create_gem_user.py
+++ openquakeplatform/openquake/management/commands/create_gem_user.py
@@ -5,19 +5,21 @@ from django.contrib.auth import get_user_model
 class Command(BaseCommand):
 
     def handle(*args, **options):
+
+        ## create Gem user without password
         User = get_user_model()
         UserGem = User.objects.create_user(username='GEM',
-                                     email='info@globalquakemodel.org',
-                                     first_name='GEM',
-                                     last_name='Foundation')
+                                           email='info@globalquakemodel.org',
+                                           first_name='GEM',
+                                           last_name='Foundation')
 
+        ## Set pwd for Gem user
         UserGem = User.objects.get(username='GEM')
         UserGem.set_password('GEM')
         UserGem.save()
 
-
+        ## Print if create Gem user is successfully 
         if UserGem.first_name == 'GEM':
             print('GEM user created')
         else:
             raise ValueError
-
