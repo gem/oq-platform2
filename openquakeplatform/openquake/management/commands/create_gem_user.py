eba7876e636f1ccf00732961d1a5af6ad64a83be
hascar <armando.scarpati@globalquakemodel.org>
2018-06-05 12:21:10 +0000

    added Gem user in a vulnerability editors group

--- openquakeplatform/openquake/management/commands/create_gem_user.py
+++ openquakeplatform/openquake/management/commands/create_gem_user.py
@@ -1,12 +1,13 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
-
+from django.contrib.auth.models import Group
 
 class Command(BaseCommand):
 
     def handle(*args, **options):
 
         pwgem = 'GEM'
+        groupname = 'vulnerability-editors'
 
         ## Create Gem user without password
         User = get_user_model()
@@ -19,6 +20,10 @@ class Command(BaseCommand):
         UserGem = User.objects.get(username=pwgem)
         UserGem.set_password(pwgem)
         UserGem.save()
+
+        ## Added Gen user in a group
+        UserGroup = Group.objects.get(name=groupname)
+        UserGem.groups.add(UserGroup)
 
         ## Print if create Gem user is successfully 
         if UserGem.first_name == pwgem:
