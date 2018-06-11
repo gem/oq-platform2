4a2e547a41dc9c36767bf8d2f62858d3448e22c5
hascar <armando.scarpati@globalquakemodel.org>
2018-06-11 13:01:38 +0000

    added groups each user in add_user command

--- openquakeplatform/pla_common/management/commands/add_user.py
+++ openquakeplatform/pla_common/management/commands/add_user.py
@@ -1,6 +1,7 @@
 import json
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 
 
 class Command(BaseCommand):
@@ -34,6 +35,7 @@ class Command(BaseCommand):
                         is_active = fields['is_active']
                         is_super = fields['is_superuser']
                         is_staff = fields['is_staff']
+                        groupname = fields['groups']
 
                         gem_user = User.objects.model(
                                                        username=username,
@@ -52,3 +54,7 @@ class Command(BaseCommand):
                             print('%s user created' % username)
                         else:
                             raise ValueError
+
+                        # Added user into the group assigned
+                        UserGroup = Group.objects.get(name=groupname)
+                        gem_user.groups.add(UserGroup)
