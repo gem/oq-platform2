74a77b62923c1407c37512d137fb91288a188a72
hascar <armando.scarpati@globalquakemodel.org>
2018-06-14 14:16:47 +0000

    deleted print fields documents json and added function to upload demo documents in add_documents command py

--- openquakeplatform/pla_common/management/commands/add_documents.py
+++ openquakeplatform/pla_common/management/commands/add_documents.py
@@ -1,18 +1,35 @@
 import json
 from django.core.management.base import BaseCommand
+from geonode.documents.models import Document
 
 
 class Command(BaseCommand):
-    args = '<documents_document.json>'
+    args = '<documents_document_demo.json>'
     help = ('Import documents')
 
-    def handle(self, doc_fname, *args, **options):
+    # def handle(self, doc_fname, *args, **options):
+    def handle(doc_fname, *args, **options):
+        doc_fname = ('/home/ubuntu/oq-platform2/'
+                     'openquakeplatform/dump/documents_document_demo.json')
         doc_json = open(doc_fname).read()
         doc_load = json.loads(doc_json)
-        
+
         # Print documents fields
         for doc in doc_load:
-            print('%s' % doc['pk'])
-            print('%s' % doc['model'])
-            print('%s' % doc['fields']['doc_file'])
-            print('%s' % doc['fields']['extension'])
+            docs = doc['fields']
+            doc_id = doc['pk']
+            extension = docs['extension']
+            object_id = docs['object_id']
+            doc_file = docs['doc_file']
+            title_en = doc_file.replace('documents/', '')
+            print(title_en)
+
+            # Add documents
+            newdoc = Document.objects.model(
+                                            id=doc_id,
+                                            title_en=title_en,
+                                            extension=extension,
+                                            object_id=object_id,
+                                            doc_file=doc_file
+                                            )
+            newdoc.save()
