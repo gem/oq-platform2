7ec0b0d5cbb4650811be9e64b3d05c89698c6e7e
hascar <armando.scarpati@globalquakemodel.org>
2018-06-25 14:45:35 +0000

    fixed path folder documents from oq-private

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -236,7 +236,7 @@ python ./manage.py add_user $HOME/$GIT_REPO/openquakeplatform/dump/auth_user_dem
 
 ## Add old documents
 python ./manage.py add_documents
-cp $HOME/oq-private/old_platform_documents/documents $HOME/geonode/geonode/uploaded/documents/
+cp $HOME/oq-private/old_platform_documents/documents/ $HOME/geonode/geonode/uploaded/documents/
 
 ## Add Gem category
 python ./manage.py loaddata $HOME/$GIT_REPO/openquakeplatform/dump/base_topiccategory.json
