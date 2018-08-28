e95fd6f6293ad0810c999924ca907400289e0853
Matteo Nastasi <nastasi@openquake.org>
2018-08-28 20:10:25 +0200

    more convergence with 'dump_content_bis' branch

--- openquakeplatform/pla_common/management/commands/add_user.py
+++ openquakeplatform/pla_common/management/commands/add_user.py
@@ -2,7 +2,6 @@ import json
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
-from geonode.layers.models import Style
 
 
 class Command(BaseCommand):
@@ -56,5 +55,3 @@ class Command(BaseCommand):
                 gem_user.groups.add(UserGroup)
 
             print('%s user created' % username)
-
-            Style.objects.all().delete()
