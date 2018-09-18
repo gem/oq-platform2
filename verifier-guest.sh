446a4f2e907eb2b70f58cf05f15112fa60696c99
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2018-09-18 14:35:44 +0200

    come back add_documents in the orignal position

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -301,6 +301,7 @@ $HOME/$GIT_REPO/openquakeplatform/bin/oq-gs-builder.sh populate -a ~/oq-platform
 cd ~/geonode
 mkdir -p $HOME/geonode/geonode/uploaded/
 cp -r $HOME/$GIT_REPO/openquakeplatform/common/gs_data/documents $HOME/geonode/geonode/uploaded/
+python ./manage.py add_documents
 
 ## Update layers from Geoserver to geonode
 cd ~/geonode
@@ -327,6 +328,5 @@ fi
 
 ## Stop Geonode
 cd ~/geonode
-python ./manage.py add_documents
 sudo supervisorctl stop openquake-webui
 paver -f $HOME/$GIT_REPO/pavement.py stop
