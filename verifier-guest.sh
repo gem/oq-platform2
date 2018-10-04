547bd048e88a2a74db0232fa0702fbb29944b91b
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2018-10-04 10:01:35 +0200

    fixed indentation for code to create unaccent extension in postgres

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -181,27 +181,27 @@ sudo -u postgres psql -d geonode_dev-imports -c 'GRANT ALL ON spatial_ref_sys TO
 # add unaccent extension and icompare_unaccent function into postgres
 cat << EOF | sudo -u postgres psql -d geonode_dev
 
--- NOTE: originally deployed as migration, we realized that this sql script must
---       be executed for every new installation (devel or production).
---       The script is idempotent so we decided to keep the original migration script too
-
-DROP OPERATOR IF EXISTS =~@ (character varying, character varying);
-DROP FUNCTION IF EXISTS icompare_unaccent(character varying, character varying);
-DROP EXTENSION IF EXISTS unaccent;
-
-CREATE EXTENSION unaccent;
-CREATE FUNCTION icompare_unaccent(character varying, character varying) RETURNS boolean
-    AS 'SELECT upper(unaccent(\$1)) LIKE upper(unaccent(\$2));'
-    LANGUAGE SQL
-    IMMUTABLE
-    RETURNS NULL ON NULL INPUT;
-
-CREATE OPERATOR =~@ (
-    LEFTARG = character varying,
-    RIGHTARG = character varying,
-    PROCEDURE = icompare_unaccent,
-    NEGATOR = !=~@
-);
+    -- NOTE: originally deployed as migration, we realized that this sql script must
+    --       be executed for every new installation (devel or production).
+    --       The script is idempotent so we decided to keep the original migration script too
+    
+    DROP OPERATOR IF EXISTS =~@ (character varying, character varying);
+    DROP FUNCTION IF EXISTS icompare_unaccent(character varying, character varying);
+    DROP EXTENSION IF EXISTS unaccent;
+    
+    CREATE EXTENSION unaccent;
+    CREATE FUNCTION icompare_unaccent(character varying, character varying) RETURNS boolean
+        AS 'SELECT upper(unaccent(\$1)) LIKE upper(unaccent(\$2));'
+        LANGUAGE SQL
+        IMMUTABLE
+        RETURNS NULL ON NULL INPUT;
+    
+    CREATE OPERATOR =~@ (
+        LEFTARG = character varying,
+        RIGHTARG = character varying,
+        PROCEDURE = icompare_unaccent,
+        NEGATOR = !=~@
+    );
 EOF
 
 #insert line in pg_hba.conf postgres
