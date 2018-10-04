354907d307535dc7da5e2c4857b33f26ef716445
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2018-10-04 09:57:31 +0200

    changed alter role supersuser with code to create unaccent extension in postgres

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -178,7 +178,31 @@ sudo -u postgres psql -d geonode_dev-imports -c 'CREATE EXTENSION postgis;'
 sudo -u postgres psql -d geonode_dev-imports -c 'GRANT ALL ON geometry_columns TO PUBLIC;'
 sudo -u postgres psql -d geonode_dev-imports -c 'GRANT ALL ON spatial_ref_sys TO PUBLIC;'
 
-sudo -u postgres psql -d geonode_dev -c 'ALTER ROLE geonode_dev SUPERUSER;'
+# add unaccent extension and icompare_unaccent function into postgres
+cat << EOF | sudo -u postgres psql -d geonode_dev
+
+-- NOTE: originally deployed as migration, we realized that this sql script must
+--       be executed for every new installation (devel or production).
+--       The script is idempotent so we decided to keep the original migration script too
+
+DROP OPERATOR IF EXISTS =~@ (character varying, character varying);
+DROP FUNCTION IF EXISTS icompare_unaccent(character varying, character varying);
+DROP EXTENSION IF EXISTS unaccent;
+
+CREATE EXTENSION unaccent;
+CREATE FUNCTION icompare_unaccent(character varying, character varying) RETURNS boolean
+    AS 'SELECT upper(unaccent(\$1)) LIKE upper(unaccent(\$2));'
+    LANGUAGE SQL
+    IMMUTABLE
+    RETURNS NULL ON NULL INPUT;
+
+CREATE OPERATOR =~@ (
+    LEFTARG = character varying,
+    RIGHTARG = character varying,
+    PROCEDURE = icompare_unaccent,
+    NEGATOR = !=~@
+);
+EOF
 
 #insert line in pg_hba.conf postgres
 if ! sudo head -n 1 /etc/postgresql/9.5/main/pg_hba.conf | grep -q "local \+all \+$GEO_DBUSER \+md5"; then
