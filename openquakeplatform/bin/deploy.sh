8577ba183cd0b60ab69c1f1581cb5f4a8cafcc30
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2019-02-08 15:05:18 +0100

    Revert "commented exec mapthumbnails test from deploy script"
    This reverts commit 6e32bb6af1cceac33d2c9affd664b0363e78905a.

--- openquakeplatform/bin/deploy.sh
+++ openquakeplatform/bin/deploy.sh
@@ -376,7 +376,7 @@ function oq_install() {
 
     if [ "$NO_EXEC_TEST" != "notest" ] ; then
         initialize_test
-        # exec_set_map_thumbs
+        exec_set_map_thumbs
         exec_test
     fi    
 }
