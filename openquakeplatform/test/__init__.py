6a005cf0bccfa3d90309749a6f8ddcb08422a053
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2019-02-12 10:06:59 +0100

    incremented timeout for __init__ tests and deploy script after restart tomcat

--- openquakeplatform/test/__init__.py
+++ openquakeplatform/test/__init__.py
@@ -3,7 +3,7 @@ from openquake.moon import platform_get, platform_del
 
 def setup_package():
     pla = platform_get()
-    pla.init(timeout=30)
+    pla.init(timeout=100)
 
 
 def teardown_package():
