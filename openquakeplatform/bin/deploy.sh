60af9eacb25a635f56845c5a780bc072ec323456
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2019-04-19 11:06:18 +0200

    changed version simplejson in setup.py and deleted installation from deploy script

--- openquakeplatform/bin/deploy.sh
+++ openquakeplatform/bin/deploy.sh
@@ -265,7 +265,6 @@ function apply_data() {
         geonode loaddata -v 3 --app vulnerability $HOME/oq-private/old_platform_documents/json/all_vulnerability.json
         geonode create_gem_user
     fi    
-    sudo /var/lib/geonode/env/bin/python -m pip install simplejson==2.0.9
     sudo sed -i 's/-Xmx128m/-Xmx4096m/g' /etc/default/tomcat7
     sudo service tomcat7 restart
     
