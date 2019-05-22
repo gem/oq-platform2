3980b16a4d1070754dd9074067058934f821b6f5
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2019-05-22 11:15:32 +0200

    complete test new metadata owner

--- openquakeplatform/test/keywords_owner_test.py
+++ openquakeplatform/test/keywords_owner_test.py
@@ -3,6 +3,7 @@ import os
 import unittest
 import time
 from openquake.moon import platform_get
+from selenium.webdriver.common.keys import Keys
 
 TIMEOUT = 100
 
@@ -12,6 +13,8 @@ class SetMetadataKeywordsOwnerTest(unittest.TestCase):
     def new_metadata_owner_test(self):
 
         pla = platform_get()
+
+        new_owner = '%s' % ('admin')
 
         pla.get('/documents/?limit=200&offset=0')
 
@@ -30,6 +33,23 @@ class SetMetadataKeywordsOwnerTest(unittest.TestCase):
             TIMEOUT, 1)
         edit_doc_button_meta.click()
 
+        # Click edit metadata
         edit_meta = pla.xpath_finduniq(
             "//a[normalize-space(text())='Edit']", TIMEOUT, 1)
         edit_meta.click()
+
+        # Change owner
+        remove_owner = pla.xpath_findfirst(
+            "//span[@class='remove']", TIMEOUT, 1)
+        remove_owner.click()
+
+        # edit name
+        edit_owner_field = pla.xpath_finduniq(
+            "//input[@id='id_resource-owner-autocomplete']")
+        edit_owner_field.send_keys('%s' % new_owner)
+
+        # Click update metadata
+        update_button_meta = pla.xpath_findfirst(
+            "//input[@type='submit' and @value='Update']",
+            TIMEOUT, 1)
+        update_button_meta.click()
