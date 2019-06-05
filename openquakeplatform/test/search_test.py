2fb810688a4fb745d644444521eff50091dcba28
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2019-06-05 15:22:01 +0200

    added double check search, header and left sidebar with one method

--- openquakeplatform/test/search_test.py
+++ openquakeplatform/test/search_test.py
@@ -3,48 +3,69 @@ import unittest
 from openquake.moon import platform_get
 from selenium.webdriver.common.action_chains import ActionChains
 
-TIMEOUT = '100'
+TIMEOUT = 100
+
+def search_element():
+   auto_complete_click = pla.xpath_findfirst(
+       "//span[@class='yourlabs-autocomplete']"
+       "/span[normalize-space(text())='assumpcao2014']",
+       TIMEOUT, 1)
+   actionChains = ActionChains(pla.driver)
+   actionChains.double_click(auto_complete_click).perform()
+
+   pla.wait_new_page(
+       auto_complete_click,
+       # '/search/?title__icontains=assumpcao2014&limit=100&offset=0',
+       '/search/?title__icontains=assumpcao2014',
+       timeout=10)
+
+   pla.xpath_finduniq(
+       "//a[@ng-if='item.detail_url.indexOf('/layers/') > -1' "
+       "and @href='/layers/oqplatform:assumpcao2014']",
+       TIMEOUT, 1)
 
 
 class SearchTest(unittest.TestCase):
     def search_test(self):
 
-        pla = platform_get('')
+        pla = platform_get()
+
+        pla.get()
 
         search_input = 'assumpcao2014'
 
-        # click search icon on header
+        # Search from header
+        #
+        # click search icon
         click_ico = pla.xpath_finduniq(
             "//a[@href='javascript:void(0)']"
             "/img[@src='/static/geonode/img/oq-search.png']",
-            100, 1)
+            TIMEOUT, 1)
         click_ico.click()
 
-        # write on serach input
+        # write on search input
         search = pla.xpath_findfirst(
-            "//input[@id='search_input' and placeholder='Search']",
+            "//input[@id='search_input' and @placeholder='Search']",
             TIMEOUT, 1)
         search.clear()
         search.send_keys(search_input)
 
-        auto_complete_click = pla.xpath_findfirst(
-            "//span[@class='yourlabs-autocomplete']"
-            "/span[normalize-space(text())='assumpcao2014']",
+        # launch search_element method
+        search_element()
+
+        # Search from left sidebar
+        #
+        # page list maps
+        pla.get('/maps')
+
+        # write on search input
+        left_search = pla.xpath_findfirst(
+            "//input[@id='text_search_input'"
+            " and @placeholder='Search by text']",
             TIMEOUT, 1)
-        actionChains = ActionChains(pla.driver)
-        actionChains.double_click(auto_complete_click).perform()
+        left_search.clear()
+        left_search.send_keys(search_input)
 
-        pla.wait_new_page(
-            auto_complete_click,
-            'search/?title__icontains=assumpcao2014&limit=100&offset=0',
-            timeout=10)
+        # launch search_element method
+        search_element()
 
-        # check title and content in search page
-        pla.xpath_finduniq(
-            "//h2[normalize-space(text())='assumpcao2014']",
-            100, 1)
-
-        pla.xpath_finduniq(
-            "//a[@ng-if='item.detail_url.indexOf('/layers/') > -1' "
-            "and @href='/layers/oqplatform:assumpcao2014']",
-            100, 1)
