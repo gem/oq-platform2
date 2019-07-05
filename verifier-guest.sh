930d1584e9fb5703bbc6e5f020ba4129ff9c1af1
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2019-07-05 14:42:23 +0200

    deleted branch in pip install django-nested-inlines for dev installation

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -52,7 +52,7 @@ extra_deps_install() {
     pip install django-nested-inline
     pip install django_extras
     pip install -e git+git://github.com/gem/django-chained-selectbox.git@pla26#egg=django-chained-selectbox-0.2.2
-    pip install -e git+git://github.com/gem/django-nested-inlines.git@pla26#egg=django-nested-inlines-0.1.4
+    pip install -e git+git://github.com/gem/django-nested-inlines.git#egg=django-nested-inlines-0.1.4
     pip install -e git+git://github.com/gem/django-chained-multi-checkboxes.git@pla26#egg=django-chained-multi-checkboxes-0.4.1
     pip install -e git+git://github.com/gem/wadofstuff-django-serializers.git@pla26#egg=wadofstuff-django-serializers-1.1.2
     pip install django-request==1.5.2
