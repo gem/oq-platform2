ec15c88538fb24eab8632aa5737868b50f54250d
hascar <arm.scar@gmail.com>
2019-07-10 12:58:28 +0000

    added test_migration for each model

--- openquakeplatform/migrations_test.py
+++ openquakeplatform/migrations_test.py
@@ -1,17 +1,41 @@
-import unittest
+# import unittest
 from django_migration_testcase import MigrationTest
 
-@unittest.skip("temporarily disabled")
+# @unittest.skip("temporarily disabled")
 class VulnMigrationTest(MigrationTest):
 
     app_name = 'vulnerability'
     before = '0001_initial'
-    after = '0003_auto_20190704_0901'
+    after = '0002_func_dist_shape_def_val_ctx'
 
-    def test_migration(self):
+    def test_migration_model_1(self):
 
-        MyModel = self.get_model_before('funcdistrdtldiscr')
+        MyModel_1 = self.get_model_before('funcdistrdtldiscr')
 
         self.run_migration()
 
-        MyModel = self.get_model_after('funcdistrdtldiscr')
+        MyModel_1 = self.get_model_after('funcdistrdtldiscr')
+
+    def test_migration_model_2(self):
+
+        MyModel_2 = self.get_model_before('funcdistrfragcont')
+
+        self.run_migration()
+
+        MyModel_2 = self.get_model_after('funcdistrfragcont')
+
+    def test_migration_model_3(self):
+
+        MyModel_3 = self.get_model_before('funcdistrvulncont')
+
+        self.run_migration()
+
+        MyModel_3 = self.get_model_after('funcdistrvulncont')
+
+    def test_migration_model_4(self):
+
+        MyModel_4 = self.get_model_before('funcdistrvulndiscr')
+
+        self.run_migration()
+
+        MyModel_4 = self.get_model_after('funcdistrvulndiscr')
