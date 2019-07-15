fe292376f1621aa3c9098e9529baf0208777c818
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2019-07-15 15:15:23 +0200

    changed migration_test py

--- openquakeplatform/migrations_test.py
+++ openquakeplatform/migrations_test.py
@@ -1,41 +1,61 @@
 # import unittest
 from django_migration_testcase import MigrationTest
+from django.contrib.auth import get_user_model
 
-@unittest.skip("temporarily disabled")
+
+# @unittest.skip("temporarily disabled")
 class VulnMigrationTest(MigrationTest):
 
     app_name = 'vulnerability'
     before = '0001_initial'
     after = '0002_func_dist_shape_def_val_ctx'
 
-    def test_migration_model_1(self):
+    def test_migration_model(self):
 
-        MyModel_1 = self.get_model_before('funcdistrdtldiscr')
+        User = get_user_model()
+        user = User.objects.model(
+                                  username="admin",
+                                  password="admin",
+                                  is_superuser="true"
+                                 )
+        user.save()
+        owner = User.objects.get(username="admin")
+        print(owner.pk)
+
+        generalinformation = self.get_model_before('generalinformation')
+        newgen = generalinformation(
+                                    owner_id=owner.pk,
+                                    name="9 Storey Non-Ductile RC-MRFs",
+                                    category='10',
+                                    material=None,
+                                    type_of_assessment="10",
+                                    article_title="Influence horizontal",
+                                    structure_type="10",
+                                    year="2016"
+                                   )
+        newgen.save()
+
+        # fragilityfunc = self.get_model_before('fragilityfunc')
+        # newfragilityfunc = fragilityfunc(
+        #                                  owner=user,
+        #                                  general_information_id=1000,
+        #                                  method_of_estimation="2",
+        #                                  func_distr_type="2"
+        #                                 )
+        # newfragilityfunc.save()
+
+        # funcdistrdtldiscr = self.get_model_before('funcdistrdtldiscr')
+        # newrec = funcdistrdtldiscr(
+        #                            owner=user,
+        #                            damage_to_loss_func_id='1000',
+        #                            # var_mean_val='',
+        #                            # var_val_coeff='',
+        #                            # func_distr_shape=''
+        #                           )
+        # newrec.save()
 
         self.run_migration()
 
-        MyModel_1 = self.get_model_after('funcdistrdtldiscr')
-
-    def test_migration_model_2(self):
-
-        MyModel_2 = self.get_model_before('funcdistrfragcont')
-
-        self.run_migration()
-
-        MyModel_2 = self.get_model_after('funcdistrfragcont')
-
-    def test_migration_model_3(self):
-
-        MyModel_3 = self.get_model_before('funcdistrvulncont')
-
-        self.run_migration()
-
-        MyModel_3 = self.get_model_after('funcdistrvulncont')
-
-    def test_migration_model_4(self):
-
-        MyModel_4 = self.get_model_before('funcdistrvulndiscr')
-
-        self.run_migration()
-
-        MyModel_4 = self.get_model_after('funcdistrvulndiscr')
+        generalinformation = self.get_model_after('generalinformation')
+        # fragilityfunc = self.get_model_after('funcdistrdtldiscr')
+        # funcdistrdtldiscr = self.get_model_after('funcdistrdtldiscr')
