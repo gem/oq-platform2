0f46c9c3a2f2679d2abc871b2684e93a4afdb42d
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2019-07-23 16:50:22 +0200

    added DamageToLossFunc and FuncDistrDTLDiscr models in migration_test

--- openquakeplatform/migrations_test.py
+++ openquakeplatform/migrations_test.py
@@ -1,7 +1,9 @@
 # import unittest
 from django_migration_testcase import MigrationTest
+from openquakeplatform.vulnerability.models import GeneralInformation
+from openquakeplatform.vulnerability.models import DamageToLossFunc
+from openquakeplatform.vulnerability.models import FuncDistrDTLDiscr
 from django.contrib.auth import get_user_model
-# from openquakeplatform.vulnerability.models import GeneralInformation
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "local_settings")
 
@@ -25,7 +27,7 @@ class VulnMigrationTest(MigrationTest):
         owner = User.objects.get(username="admin")
         print('Pk: %s' % owner.pk)
 
-        geninformation = self.get_model_before('generalinformation')
+        geninformation = self.get_model_before('GeneralInformation')
         newgen = geninformation(
                                 owner_id=owner.pk,
                                 name="9 Storey Non-Ductile RC-MRFs",
@@ -38,11 +40,50 @@ class VulnMigrationTest(MigrationTest):
                                )
         newgen.save()
 
+        newgen_istance = GeneralInformation.objects.get(
+            name="9 Storey Non-Ductile RC-MRFs")
+        print(newgen_istance.pk)
+
+        damloss = self.get_model_before('DamageToLossFunc')
+
+        newdamloss = damloss(
+                             owner_id=owner.pk,
+                             general_information_id=newgen_istance.pk,
+                             method_of_estimation="2",
+                             damage_scale="6",
+                             # limit_states_desc=None,
+                             resp_var="5"
+                             )
+        newdamloss.save()
+
+        newdamloss_istance = DamageToLossFunc.objects.get(
+            general_information=newgen_istance.pk)
+
+        dtldiscr = self.get_model_before('FuncDistrDTLDiscr')
+        newdtldiscr = dtldiscr(
+                               owner_id=owner.pk,
+                               damage_to_loss_func_id=newdamloss_istance.pk,
+                               var_mean_val="0.1;0.2;0.4;0.9;1",
+                               # var_val_coeff="",
+                               func_distr_shape="1"
+                               )
+        newdtldiscr.save()
+
         # import pdb;pdb.set_trace()
         self.run_migration()
 
-        geninformation = self.get_model_after('generalinformation')
+        geninformation = self.get_model_after('GeneralInformation')
+        damloss = self.get_model_after('DamageToLossFunc')
+        dtldiscr = self.get_model_after('FuncDistrDTLDiscr')
 
-        new_gen = geninformation.objects.get(
+        new_gen = GeneralInformation.objects.get(
             name="9 Storey Non-Ductile RC-MRFs")
-        print('Function: %s' % new_gen.pk)
+        print('GeneralInformation Id: %s' % new_gen.pk)
+
+        new_damloss = DamageToLossFunc.objects.get(
+            general_information_id=new_gen.pk)
+        print('DamageToLossFunc Id: %s' % new_damloss.pk)
+
+        new_dtldiscr = FuncDistrDTLDiscr.objects.get(
+            damage_to_loss_func_id=new_damloss.pk)
+        print('FuncDistrDTLDiscr Id: %s' % new_dtldiscr.pk)
