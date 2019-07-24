09112fe914955ba47cd9f01d9e8889281edaa09a
Matteo Nastasi <nastasi@openquake.org>
2019-07-24 10:49:17 +0200

    update version to 2.3.0

--- create_gem_requirements.sh
+++ create_gem_requirements.sh
@@ -1,14 +1,14 @@
-#!/bin/bash                                                                 
+#!/bin/bash
 IFS='
 '
 ( for l in $(pip freeze | grep -v "github.com/GeoNode/geonode.git"); do
-     
+
     name="$(echo "$l" | sed 's/==.*//g')"
     major="$(echo "$l" | sed 's/^.*==//g;s/^\([^\.]*\).*/\1/g')"
     minor="$(echo "$l" | sed 's/^.*==//g;s/^[^\.]*\.\([^\.]*\).*/\1/g')"
     minornum="$(echo "$minor" | sed 's/[^0-9].*//g')"
     rest="$(echo "$l" | sed 's/^.*==//g;s/^[^\.]*\.[^\.]*//g')"
-     
+
     echo "$name >= $major.$minor$rest , < $major.$((minornum + 1))"
 
 done ) > gem_geonode_requirements.txt
