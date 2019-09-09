86bffe99efaf7d4a9a2f15ff190eead93e74b7ab
Matteo Nastasi <nastasi@openquake.org>
2019-09-09 15:48:27 +0200

    small cleanup

--- openquakeplatform/__init__.py
+++ openquakeplatform/__init__.py
@@ -12,8 +12,6 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# from __future__ import absolute_import
-# from .version import git_suffix
 from __future__ import absolute_import
 from .version import git_suffix
 
