a01a7e608bf2badb0a51e07625ac8e46766101ba
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2019-10-03 17:00:40 +0200

    return after migration 004.. in migrations_test

--- openquakeplatform/migrations_test.py
+++ openquakeplatform/migrations_test.py
@@ -9,7 +9,7 @@ class VulnMigrationTest(MigrationTest):
 
     app_name = 'vulnerability'
     before = '0001_initial'
-    after = '0005_auto_20191003_1406.py'
+    after = '0004_func_dist_vuln_frag_dtl_cont'
 
     def test_migration_model(self):
 
