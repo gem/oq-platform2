2be84288c3a349911609def8a1f71f3c2ba9473c
armando.scarpati <armando.scarpati@globalquakemodel.org>
2020-01-13 10:05:08 +0100

    added install setuptools for deploy

--- openquakeplatform/bin/deploy.sh
+++ openquakeplatform/bin/deploy.sh
@@ -46,6 +46,8 @@ sudo mkdir -p /var/lib/geonode/env
 sudo virtualenv /var/lib/geonode/env
 source /var/lib/geonode/env/bin/activate
 
+sudo /var/lib/geonode/env/bin/python -m pip install "setuptools==44.0.0"
+
 sudo /var/lib/geonode/env/bin/python -m pip install "numpy>=1.16,<1.17"
 
 sudo /var/lib/geonode/env/bin/python -m pip install "django<2"
