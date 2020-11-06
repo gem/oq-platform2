18b8b2df1f9790c93d1bbfde047ffd5f20c8b6cb
armando.scarpati <armando.scarpati@globalquakemodel.org>
2020-11-06 17:40:35 +0100

    added encoding utf8 management

--- openquakeplatform/pla_common/management/commands/extract_users.py
+++ openquakeplatform/pla_common/management/commands/extract_users.py
@@ -1,18 +1,20 @@
 import os
 import csv
+import codecs
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
 
 class Command(BaseCommand):
+    args = '<csv name>'
     help = ('Extract users')
 
     def handle(self, user_fname, *args, **options):
 
-        with open(user_fname, 'w', newline='') as csvfile:
+        with open(user_fname, 'wb') as csvfile:
             fieldnames = ['email', 'full_name', 'is_active', 'is_superuser', 'is_staff']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writerheader()
+            writer.writeheader()
 
             User = get_user_model()
 
@@ -20,12 +22,12 @@ class Command(BaseCommand):
 
             for user in users:
 
-                email = user.email
-                first_name = user.first_name
-                last_name = user.last_name
-                is_active = user.is_active
-                is_superuser = user.is_superuser
-                is_staff = user.is_staff
+                email = user.email.encode('utf8')
+                first_name = user.first_name.encode('utf8')
+                last_name = user.last_name.encode('utf8')
+                is_active = str(user.is_active)
+                is_superuser = str(user.is_superuser)
+                is_staff = str(user.is_staff)
 
                 if first_name is not '' and last_name is not '':
                     full_name = '%s %s' % (first_name, last_name)
